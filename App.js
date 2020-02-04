d551bb6eb22fe735a52c54212f0e608726e91996
Vidyanand Sagar <sagar.vivek159@gmail.com>
2020-02-04 14:07:04 +0530

    Added all reqd, just modification of Ui need to be done

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
-import { View, Text, StyleSheet, FlatList , Image, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity, Button, Alert } from 'react-native';
 import Circle from './circle';
+import * as Progress from 'react-native-progress';
+
 const formatData = (data, numColumns) => {
   const numberOfFullRows = Math.floor(data.length / numColumns);
 
@@ -55,89 +57,153 @@ const data = [
     "word": "D"
   }
 ]
-const word = ["APPLE","HEAD","PLACE","SPACE","HELP"];
-let showData = [];
-
 export default class App extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      clickedData:''
+      clickedData: '',
+      wordCount: 0,
+      letterCount: 0,
+      addedWord: false,
+      showData: []
     };
   }
 
-  addData =(data)=>{
-    let add = this.state.clickedData.concat(data);
-    this.setState({clickedData:add})
-    console.log(word.find((data)=> data===this.state.clickedData));
-    
-    if(word.find((data)=> data===this.state.clickedData)){
-      alert('Good')
-      let wordAdd ={
-        word:this.state.clickedData,
-        count: this.state.clickedData.length
-      };
-      showData.push(wordAdd);
-      console.log(showData);
-      this.setState({clickedData:''})
-    }
-    console.log(this.state.clickedData)
-}
+  addData = (data) => {
+    this.setState((prevState, props) => {
+      return { clickedData: prevState.clickedData + data };
+    })
+  }
 
-renderItem1 = ({item}) =>{
-  return(
-    <View style={{height:70, flexDirection:'row'}}>
-      <View style={{padding:10,flex:2, justifyContent:'center'}}>
-        <Text style={{fontSize:25}}>{item.word}</Text>
+  renderItem1 = ({ item }) => {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ alignSelf: 'flex-start', justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ fontSize: 25 }}>{item.word}</Text>
+        </View>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Circle word={item.count} />
+        </View>
       </View>
-      <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-        <Circle word={item.count}/>
-      </View>
-    </View>
-  )
-}
+    )
+  }
 
   renderItem = ({ item }) => {
     return (
-      <TouchableOpacity onPress={()=>{
+      <TouchableOpacity onPress={() => {
         // alert(item.word);
         this.addData(item.word)
       }}>
-          <Circle word={item.word} color={'red'}/>
+        <Circle word={item.word} color={'red'} />
       </TouchableOpacity>
     );
   };
- 
+
+  cancelled = () => {
+    this.setState({
+      clickedData: '',
+      typedWord: ''
+    })
+  }
+
+  pushData = () => {
+    let wordAdd = {
+      word: this.state.clickedData,
+      count: this.state.clickedData.length
+    };
+    this.setState((prevState, props) => {
+      return {
+        wordCount: prevState.wordCount + 1,
+        clickedData: '',
+        showData: [...this.state.showData, wordAdd]
+      };
+    })
+  }
+
+  onOkPressed = () => {
+    this.setState((prevState, props) => {
+      return {
+        wordCount: 0,
+        clickedData: '',
+        showData: []
+      };
+    });
+  }
+
   render() {
+    if (this.state.wordCount === 5) {
+      return (
+        <View>
+          <Text style={{ fontSize: 35, color: 'red' }}>Give me Five</Text>
+          {Alert.alert(
+            'Successfully Submitted...!!',
+            'Restart the application...!!',
+            [
+              { text: 'Cancel', onPress: () => console.log('Cancel Pressed!') },
+              { text: 'OK', onPress: this.onOkPressed },
+            ],
+            { cancelable: false }
+          )}
+        </View>
+      )
+    }
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Text style={{ fontSize: 35, color:'red' }}>Give me Five</Text>
+          <Text style={{ fontSize: 35, color: 'red' }}>Give me Five</Text>
+        </View>
+        <View>
+          <Progress.Bar
+            style={styles.progBar}
+            progress={this.state.showData.length / 5}
+            width={null}
+            color="red"
+            unfilledColor="#ffffff"
+          />
         </View>
         <View style={styles.center}>
           <View style={styles.top}>
             <FlatList
               data={formatData(data, numColumns)}
-              style={{padding:5}}
               renderItem={this.renderItem}
               numColumns={numColumns}
             />
           </View>
-          <View style={styles.bottom}>
-          <FlatList
-              data={showData}
-              style={{padding:5}}
+          {this.state.clickedData !== '' ?
+            <View style={{ flex: 0.5, flexDirection: 'row' }}>
+              <View style={styles.bottom}>
+                <Text>{`${this.state.clickedData}`}</Text>
+              </View>
+              <View style={{
+                flexDirection: 'row'
+              }}>
+                <Button
+                  title="Cancel"
+                  onPress={() => this.cancelled()}
+                  style={{ color: 'red', borderRadius: 25, marginRight: 5 }}
+                />
+                <Button
+                  title="OK"
+                  onPress={() => this.pushData()}
+                />
+              </View>
+            </View> : null
+          }
+          <View style={{ flex: 2 }}>
+            <FlatList
+              data={this.state.showData}
+              style={{ padding: 5 }}
               renderItem={this.renderItem1}
-            />   
+            />
           </View>
         </View>
-        <View style={styles.footer}>
-        <Image
-          style={{width:"100%",height: 200,resizeMode: 'cover'}}
-          source={require('./assets/pick.png')}
-        />
+        <View style={{ flex: 3 }}>
+          <Image
+            style={{ width: "100%", height: 200, resizeMode: 'cover' }}
+            source={require('./assets/pick.png')}
+          />
         </View>
-      </View>
+      </View >
     );
   }
 }
@@ -146,25 +212,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  progBar: {
+    borderRadius: 0,
+    borderWidth: 0,
+    marginLeft: -1,
+    padding: 0
+  },
   header: {
     flex: 0.5,
-    paddingLeft: 25,
-    justifyContent: 'flex-end',
-    borderBottomWidth:10,
-    borderBottomColor:'red'
+    paddingLeft: 15
   },
   center: {
-    flex: 3
+    flex: 5
   },
   top: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly'
+    flex: 2,
+    paddingVertical: 5
   },
   bottom: {
-    flex: 1.7
+    flex: 0.5
   },
-  footer: {
-    flex: 1.45,
-  }
 })
